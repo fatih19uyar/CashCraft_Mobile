03ff83f79549b9f364f5bc4c6d3c25e24dc72f4b
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-02 12:41:07 +0200

    WelcomeScreen updated.

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import WelcomeScreenForm from '../screenForms/HomeScreenForm';
+import React, {useEffect, useState} from 'react';
+import WelcomeScreenForm from '../screenForms/WelcomeScreenForm';
+import LoadingScreen from '../components/LoadingScreen';
 
 type WelcomeScreenProps = {navigation: any};
 
 const WelcomeScreen: React.FC<WelcomeScreenProps> = ({navigation}) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <WelcomeScreenForm
-      onSign={() => navigation.navigate('LoginScreen')}
-      onRegister={() => navigation.navigate('RegisterScreen')}
-    />
+    <>
+      <WelcomeScreenForm
+        onSign={() => navigation.navigate('LoginScreen')}
+        onRegister={() => navigation.navigate('RegisterScreen')}
+      />
+      <LoadingScreen visible={loading} />
+    </>
   );
 };
 
