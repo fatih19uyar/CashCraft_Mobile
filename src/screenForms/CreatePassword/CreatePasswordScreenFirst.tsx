8c8bf71c74c6ead3c31d2be0c7d0d4b2beeb0ab2
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-05 03:46:20 +0200

    PasswordForm added;

---
+++ src/screenForms/CreatePassword/CreatePasswordScreenFirst.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import {
+  Field,
+  InjectedFormProps,
+  formValueSelector,
+  reduxForm,
+} from 'redux-form';
+import {ConnectedProps, connect} from 'react-redux';
+import PressButton from '../../components/PressButton';
+import Background from '../../components/Background';
+import TextView from '../../components/TextView';
+import PasswordInput from '../../components/PasswordInput';
+interface IProps extends ConnectedProps<typeof connector> {
+  goToNextForm: (values: any) => void;
+}
+const PasswordInputField = ({input}: any) => {
+  return <PasswordInput length={6} onChangePassword={input.onChange} />;
+};
+const CreatePasswordScreenFirst: React.FC<
+  IProps & InjectedFormProps<{}, IProps>
+> = ({handleSubmit, goToNextForm}) => (
+  <>
+    <Background>
+      <TextView
+        textColor={'black'}
+        textSize={39}
+        text={'6 Haneli Şifre Oluştur'}
+        textStyle={'500'}
+        textMargin={{top: 20, bottom: 50}}
+      />
+      <TextView
+        textColor={'black'}
+        textSize={18}
+        text={'Şifreniz yalnızca rakamlardan oluşmalıdır.'}
+        textStyle={'normal'}
+        textMargin={{top: 0, bottom: 0}}
+      />
+      <Field name="createPassword" component={PasswordInputField} />
+      <PressButton
+        onPress={handleSubmit(goToNextForm)}
+        textColor=""
+        text="Devam Et"
+        mode="Button2"
+      />
+    </Background>
+  </>
+);
+
+const selector = formValueSelector('createPassword');
+const mapStateToProps = (state: any) => {
+  const password = selector(state, 'password');
+  return {
+    password,
+  };
+};
+const connector = connect(mapStateToProps);
+export default connector(
+  reduxForm<{}, IProps>({
+    form: 'createPassword',
+    destroyOnUnmount: false,
+    forceUnregisterOnUnmount: true,
+  })(CreatePasswordScreenFirst),
+);
---
+++ src/screenForms/CreatePassword/CreatePasswordScreenSecond.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import {
+  Field,
+  InjectedFormProps,
+  formValueSelector,
+  reduxForm,
+} from 'redux-form';
+import {ConnectedProps, connect} from 'react-redux';
+import PressButton from '../../components/PressButton';
+import Background from '../../components/Background';
+import TextView from '../../components/TextView';
+import PasswordInput from '../../components/PasswordInput';
+interface IProps extends ConnectedProps<typeof connector> {
+  goScreen: (values: any) => void;
+}
+const PasswordInputField = ({input}: any) => {
+  return <PasswordInput length={6} onChangePassword={input.onChange} />;
+};
+const CreatePasswordScreenSecond: React.FC<
+  IProps & InjectedFormProps<{}, IProps>
+> = ({handleSubmit, goScreen}) => (
+  <>
+    <Background>
+      <TextView
+        textColor={'black'}
+        textSize={39}
+        text={'Şifreyi Tekrar Giriniz'}
+        textStyle={'500'}
+        textMargin={{top: 20, bottom: 50}}
+      />
+      <TextView
+        textColor={'black'}
+        textSize={18}
+        text={'Şifreniz yalnızca rakamlardan oluşmalıdır.'}
+        textStyle={'normal'}
+        textMargin={{top: 0, bottom: 0}}
+      />
+      <Field name="reCreatePassword" component={PasswordInputField} />
+      <PressButton
+        onPress={handleSubmit(goScreen)}
+        textColor=""
+        text="Devam Et"
+        mode="Button2"
+      />
+    </Background>
+  </>
+);
+
+const selector = formValueSelector('createPassword');
+const mapStateToProps = (state: any) => {
+  const password = selector(state, 'reCreatePassword');
+  return {
+    password,
+  };
+};
+const connector = connect(mapStateToProps);
+export default connector(
+  reduxForm<{}, IProps>({
+    form: 'createPassword',
+    destroyOnUnmount: false,
+    forceUnregisterOnUnmount: true,
+  })(CreatePasswordScreenSecond),
+);
