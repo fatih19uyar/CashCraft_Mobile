16c00c4bcf7909f3c3d2f8b8b6ede8b3113a013a
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-07 16:06:56 +0200

    colors & values updated.

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,10 +2,12 @@ const colors = {
   background: '#D6D6D6',
   text: '#000000',
   white: '#FFFFFF',
+  gray: '#717171',
   buttonPrimary: '#44B978',
   buttonSecondary: '#A6E7C3',
   inputTextBackground: '#F5F5F5',
   inputTextOutline: '#000000',
+  transactionListAmount: '#005F2A',
   // Diğer renkleri buraya ekleyin !
 };
 
---
+++ src/values/values.ts
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export const [items] = React.useState([
+  {
+    key: 1,
+    name: 'Alışveriş',
+    amount: 356,
+    date: '2023-07-29',
+  },
+  {
+    key: 2,
+    name: 'Fatura Ödeme',
+    amount: 262,
+    date: '2023-07-30',
+  },
+  {
+    key: 3,
+    name: 'Transfer',
+    amount: 159,
+    date: '2023-07-28',
+  },
+  {
+    key: 4,
+    name: 'Ödeme',
+    amount: 333,
+    date: '2023-07-28',
+  },
+]);
+export const campaigns = [
+  {
+    campName: 'Kampanya 1',
+    campImg: '../assets/campaign.png',
+    campDetails: 'Kampanya 1 Detayları',
+  },
+  {
+    campName: 'Kampanya 2',
+    campImg: '../assets/campaign.png',
+    campDetails: 'Kampanya 2 Detayları',
+  },
+  {
+    campName: 'Kampanya 3',
+    campImg: '../assets/campaign.png',
+    campDetails: 'Kampanya 3 Detayları',
+  },
+];
