ad558d54ffd81933be02840c48cd7cd765231775
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-10 16:17:16 +0200

    Stacks updated.

--- src/navigations/AppStack.tsx
+++ src/navigations/AppStack.tsx
@@ -1,44 +1,21 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import ProfileScreen from '../screens/ProfileScreen';
 import HomeScreen from '../screens/HomeScreen';
-import QRPaymentScreen from '../screens/QRPaymentScreen';
-import WalletScreen from '../screens/WalletScreen';
-import {Image} from 'react-native'; // Image bileşenini içe aktardık
-import themes from '../utils/themes';
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const AppStack = () => {
+const HomeStack = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
+    <Stack.Navigator
+      screenOptions={{
         headerShown: false,
-        tabBarStyle: {
-          borderTopColor: themes.light.colors.borderTopColor,
-          borderTopWidth: themes.light.border.small,
-        },
-        tabBarIcon: ({color, size}) => {
-          let iconName;
-          if (route.name === 'Ana Sayfa') {
-            iconName = require('../assets/home_icon.png'); // İlgili ad için ikonun yolu
-          } else if (route.name === 'QR Ödeme') {
-            iconName = require('../assets/qr_code_icon.png'); // İlgili ad için ikonun yolu
-          } else if (route.name === 'Cüzdan') {
-            iconName = require('../assets/wallet_icon.png'); // İlgili ad için ikonun yolu
-          }
-          return (
-            <Image
-              source={iconName}
-              style={{width: size, height: size, tintColor: color}}
-            />
-          );
-        },
-      })}>
-      <Tab.Screen name="Ana Sayfa" component={HomeScreen} />
-      <Tab.Screen name="QR Ödeme" component={QRPaymentScreen} />
-      <Tab.Screen name="Cüzdan" component={WalletScreen} />
-    </Tab.Navigator>
+      }}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+    </Stack.Navigator>
   );
 };
 
-export default AppStack;
+export default HomeStack;
---
+++ src/navigations/TabBottomStack.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import QRPaymentScreen from '../screens/QRPaymentScreen';
+import WalletScreen from '../screens/WalletScreen';
+import {Image} from 'react-native'; // Image bileşenini içe aktardık
+import themes from '../utils/themes';
+import AppStack from './AppStack';
+import HomeStack from './AppStack';
+
+const Tab = createBottomTabNavigator();
+
+const TopBottomStack = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarStyle: {
+          borderTopColor: themes.light.colors.borderTopColor,
+          borderTopWidth: themes.light.border.small,
+        },
+        tabBarIcon: ({color, size}) => {
+          let iconName;
+          if (route.name === 'Ana Sayfa') {
+            iconName = require('../assets/home_icon.png'); // İlgili ad için ikonun yolu
+          } else if (route.name === 'QR Ödeme') {
+            iconName = require('../assets/qr_code_icon.png'); // İlgili ad için ikonun yolu
+          } else if (route.name === 'Cüzdan') {
+            iconName = require('../assets/wallet_icon.png'); // İlgili ad için ikonun yolu
+          }
+          return (
+            <Image
+              source={iconName}
+              style={{width: size, height: size, tintColor: color}}
+            />
+          );
+        },
+      })}>
+      <Tab.Screen name="Ana Sayfa" component={HomeStack} />
+      <Tab.Screen name="QR Ödeme" component={QRPaymentScreen} />
+      <Tab.Screen name="Cüzdan" component={WalletScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default TopBottomStack;
