f4d0d6b264264011803f5c09ec564fad686e8f0a
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-10 02:17:15 +0200

    QRPayment & Wallet screen added.

---
+++ src/screens/QRPaymentScreen.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const QRPaymentScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>QRPaymentScreen</Text>
+    </View>
+  );
+};
+
+export default QRPaymentScreen;
---
+++ src/screens/WalletScreen.tsx
@@ -0,0 +1,18 @@
+import React, {useRef} from 'react';
+import {View, Text, Button} from 'react-native';
+import ButtonModal from '../components/ProfileMenu';
+
+const WalletScreen = () => {
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}></View>
+    </>
+  );
+};
+
+export default WalletScreen;
