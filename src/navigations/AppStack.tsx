c4ba4514a450edb8ec6b689f2c66a17543c57d23
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-10 02:15:53 +0200

    Bottom nav. bar added.

--- src/navigations/AppStack.tsx
+++ src/navigations/AppStack.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
-
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import QRPaymentScreen from '../screens/QRPaymentScreen';
+import WalletScreen from '../screens/WalletScreen';
+import {Image} from 'react-native'; // Image bileşenini içe aktardık
+import themes from '../utils/themes';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
+    <Tab.Navigator
+      screenOptions={({route}) => ({
         headerShown: false,
-      }}>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-    </Stack.Navigator>
+        tabBarStyle: {
+          borderTopColor: themes.light.colors.borderTopColor,
+          borderTopWidth: themes.light.border.small,
+        },
+        tabBarIcon: ({color, size}) => {
+          let iconName;
+          if (route.name === 'Ana Sayfa') {
+            iconName = require('../assets/home_icon.png'); // İlgili ad için ikonun yolu
+          } else if (route.name === 'QR Ödeme') {
+            iconName = require('../assets/qr_code_icon.png'); // İlgili ad için ikonun yolu
+          } else if (route.name === 'Cüzdan') {
+            iconName = require('../assets/wallet_icon.png'); // İlgili ad için ikonun yolu
+          }
+          return (
+            <Image
+              source={iconName}
+              style={{width: size, height: size, tintColor: color}}
+            />
+          );
+        },
+      })}>
+      <Tab.Screen name="Ana Sayfa" component={HomeScreen} />
+      <Tab.Screen name="QR Ödeme" component={QRPaymentScreen} />
+      <Tab.Screen name="Cüzdan" component={WalletScreen} />
+    </Tab.Navigator>
   );
 };
 
