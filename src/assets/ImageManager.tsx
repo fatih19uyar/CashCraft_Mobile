5c7dfb51ba88cbf7122d585db55476e6ee8c24c7
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-20 15:03:56 +0200

    ImageManager added.

---
+++ src/assets/ImageManager.tsx
@@ -0,0 +1,6 @@
+export const ImagesData = {
+  burgerKing: require('./CampaingsLogo/burger_king.png'),
+  uber: require('./CampaingsLogo/uber.png'),
+  trendyol: require('./CampaingsLogo/trendyol.png'),
+  defacto: require('./CampaingsLogo/defacto.jpg'),
+};
