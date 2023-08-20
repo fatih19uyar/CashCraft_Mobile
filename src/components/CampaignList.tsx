ffa839f21f26cf4bdab7dd2a589d2eaca7cb26fb
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-08-20 15:04:33 +0200

    CampaignList updated.

--- src/components/CampaignList.tsx
+++ src/components/CampaignList.tsx
@@ -1,40 +1,58 @@
 import React from 'react';
 import {View, Image, Dimensions, ScrollView} from 'react-native';
-import {Card, Text} from 'react-native-paper';
+import {Card} from 'react-native-paper';
 import styled from 'styled-components/native';
 import colors from '../utils/colors';
 import TextView from './TextView';
 import {Campaign} from '../types/type';
+import {ImagesData} from '../assets/ImageManager';
 
 interface CampaignListProps {
   campaigns: Campaign[];
+  handleCardPress: (index: number) => void;
 }
 
-const getWidth = () => {
-  const {width} = Dimensions.get('window');
-  return width; // Ekranın yatay çözünürlüğünün %90'ini hesapla
-};
+const CampaignList: React.FC<CampaignListProps> = ({
+  campaigns,
+  handleCardPress,
+}) => {
+  const cardWidth = Dimensions.get('window').width * 0.3; // Ekran genişliğinin %30'u kadar bir kart genişliği
+  const cardMargin = Dimensions.get('window').width * 0.02; // Ekran genişliğinin %2'si kadar bir kart kenar boşluğu
 
-const CampaignList: React.FC<CampaignListProps> = ({campaigns}) => {
   return (
     <>
-      <StyledScrollView horizontal>
+      <StyledScrollView style={{height: '20%'}} horizontal>
         {campaigns.map((campaign, index) => (
-          <StyledCard key={index}>
-            <StyledTextView
-              textColor={'black'}
-              textSize={getWidth() > 380 ? 10 : 22}
-              text={`${campaign.campName}`}
-              textStyle={'bold'}
-              textMargin={{top: 0, bottom: 0}}
-            />
-            <StyledTextView
-              textColor={'black'}
-              textSize={12}
-              text={`${campaign.campDetails}`}
-              textStyle={'500'}
-              textMargin={{top: 5, bottom: 0}}
-            />
+          <StyledCard
+            key={index}
+            style={{
+              width: cardWidth,
+              marginLeft: index === 0 ? cardMargin : 0, // İlk kartın sol kenar boşluğu
+              marginRight: cardMargin, // Kartlar arası sağ kenar boşluğu
+              height: 150,
+            }}
+            onPress={() => handleCardPress(index)} // onPress eventini handleCardPress fonksiyonu ile yönlendiriyoruz
+          >
+            <CardInnerWrapper>
+              <StyledImage
+                style={{width: 40, height: 40}}
+                source={campaign.campImg} // require ile dosya yolu kullanma
+              />
+              <TextView
+                textColor={'black'}
+                textSize={15}
+                text={`${campaign.campName}`}
+                textStyle={'bold'}
+                textMargin={{top: 5, bottom: 0}}
+              />
+              <TextView
+                textColor={'black'}
+                textSize={12}
+                text={`${campaign.campTitle}`}
+                textStyle={'300'}
+                textMargin={{top: 5, bottom: 0}}
+              />
+            </CardInnerWrapper>
           </StyledCard>
         ))}
       </StyledScrollView>
@@ -43,31 +61,23 @@ const CampaignList: React.FC<CampaignListProps> = ({campaigns}) => {
 };
 
 const StyledCard = styled(Card)`
-  width: 28%;
-  height: 95%;
   border-width: ${({theme}) => theme.border.small}px;
   border-radius: ${({theme}) => theme.radius.large}px;
   border-bottom-width: ${({theme}) => theme.border.extraLarge}px;
   border-color: ${colors.buttonPrimary};
-  justify-content: center;
-  margin-horizontal: ${({theme}) => theme.itemMargin.small}px;
 `;
 const StyledScrollView = styled(ScrollView)`
   flex-direction: row;
   padding: 3px;
 `;
+const CardInnerWrapper = styled(View)`
+  padding: 5px;
+  align-items: center;
+  justify-content: center;
+`;
 const StyledImage = styled(Image)`
   width: 40px;
   height: 40px;
-`;
-
-const StyledTextView = styled(TextView)`
-  font-size: ${({theme}) => theme.textSize}px;
-`;
-
-const StyledText = styled(Text)`
-  font-weight: bold;
-  text-align: left;
 `;
 
 export default CampaignList;
