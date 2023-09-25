24fe30b05a61dd4d92d7dec3c2d7c21661a2dd73
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-09-25 13:12:45 +0200

    Http request permissons fixed.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,8 @@
-import axios, {AxiosRequestConfig} from 'axios';
+import axios from 'axios';
 import config from '../../config';
 
-const API_BASE_URL = config.BASE_URL;
 // Axios yapılandırması
 export const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: config.BASE_URL,
   timeout: 10000, // İstek zaman aşımı süresi (ms)
 });
