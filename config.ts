9d9e43ca3caa155ad83aee7b940b53305f80db8a
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-12-15 15:55:39 +0100

    modified:   config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 const config = {
-  //BASE_URL: 'http://localhost:4000/api',
-  BASE_URL: 'https://walletserver.idvlabs.com/api/',
+  BASE_URL: 'http://localhost:4000/api',
+  //BASE_URL: 'https://walletserver.idvlabs.com/api/',
 };
 
 export default config;
